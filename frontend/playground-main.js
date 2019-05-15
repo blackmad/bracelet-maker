12a892116e7c3dc3aa095130b5b12b1941eca882
David Blackman <david@blackmad.com>
2019-05-15 00:34:24 -0400

    working reasonable conic model

--- frontend/playground-main.js
+++ frontend/playground-main.js
@@ -2,6 +2,8 @@ var SimplexNoise = require('../../simplex-noise');
 var makerjs = require('makerjs');
 
 function makeTwoEvenlySpacedBolts(p1, p2) {
+    var MillimeterToInches = 0.0393701;
+
     var RivetRadius = 2.5 * MillimeterToInches;
         
     var rivetHole = new makerjs.models.Oval(RivetRadius, RivetRadius)
@@ -36,7 +38,8 @@ function makeConicSection({
     bottomCircumference,
     height,
     widthOffset = 0,
-    heightOffset = 0
+    heightOffset = 0,
+    filletRadius = null
 }) {
     // math from: https://www.templatemaker.nl/api/api.php?client=templatemaker&request=galleryitem&template=cone&file=example-math.jpg
     // take the inputs and compute a conical secion
@@ -52,13 +55,16 @@ function makeConicSection({
     var Q = P + R; // long radius
     var alpha = Angle.fromRadians(L / P);
     
+    // For making inner/offset conic sections, this calculates how much to offset the angle
     var widthOffsetAngle = Angle.fromRadians(widthOffset / Q);
                     
+    // Compute the arcs that make up the cuff
     var cuffPaths = {
         p1: new makerjs.paths.Arc([0,0], P + heightOffset, widthOffsetAngle.degrees, alpha.degrees - widthOffsetAngle.degrees),
         p2: new makerjs.paths.Arc([0,0], Q - heightOffset, widthOffsetAngle.degrees, alpha.degrees - widthOffsetAngle.degrees)
     };
 
+    // Compute the lines that connect the two arcs
     cuffPaths['l1'] = new makerjs.paths.Line(
         makerjs.point.fromArc(cuffPaths['p1'])[0], 
         makerjs.point.fromArc(cuffPaths['p2'])[0]
@@ -68,18 +74,23 @@ function makeConicSection({
         makerjs.point.fromArc(cuffPaths['p2'])[1]
     );
 
+    // make the paths into a unified model
     var cuffChain = makerjs.model.findSingleChain({paths: cuffPaths});
     var cuffModel = makerjs.chain.toNewModel(cuffChain);
+    var fillet = null;
+    if (filletRadius) {
+        fillet = makerjs.chain.fillet(cuffChain, filletRadius);
+    }
 
     return {
-        models: {cuff: cuffModel},
-        widthOffset: widthOffset,
+        models: {cuff: cuffModel, fillet: fillet},
+        widthOffset: widthOffsetAngle,
         alpha: alpha,
+        shortRadius: P,
         longRadius: Q,
         units: makerjs.unitType.Inch
     };
 }
-
 
 function ConicCuff(
     height,
@@ -89,11 +100,13 @@ function ConicCuff(
     hash_width,
     seed
 ) {
+    /***** START OVERALL CUFF SHAPE + INNER *****/
     // Actual outer cuff cut
     var cuffModel = makeConicSection({
         topCircumference: wristWidthRaw + 1.0, 
         bottomCircumference: forearm_circumference + 0.7, 
-        height: height
+        height: height,
+        filletRadius: 0.2
     });
     // Inner "safe" area for design. Not actually printed. Used to calculate intersection of inner design.
     var cuffModelInner = makeConicSection({
@@ -110,42 +123,64 @@ function ConicCuff(
             cuffModelInner: cuffModelInner
         }
     }
+    /***** END OVERALL CUFF SHAPE + INNER *****/
 
-    // BOLTS !!!
-    console.log(cuffModel)
-    cuffModel.paths = [
-       new makerjs.paths.Line([0, 0], [cuffModel.longRadius, Math.tan(cuffModelInner.widthOffset.radians/2)*cuffModel.longRadius]),
-       new makerjs.paths.Line([0, 0], [cuffModel.longRadius, Math.tan(cuffModel.alpha.radians - (cuffModelInner.widthOffset.radians/2))*cuffModel.longRadius])
+    /***** START RIVET HOLES *****/
+    const boltGuideLine1P1 = [
+        cuffModel.shortRadius * Math.cos(cuffModelInner.widthOffset.radians/2),
+        cuffModel.shortRadius * Math.sin(cuffModelInner.widthOffset.radians/2)
     ];
-    console.log([Math.tan(cuffModelInner.widthOffsetRads/2)*cuffModel.longRadius, cuffModel.longRadius]);
+    const boltGuideLine1P2 = [
+        cuffModel.longRadius * Math.cos(cuffModelInner.widthOffset.radians/2),
+        cuffModel.longRadius * Math.sin(cuffModelInner.widthOffset.radians/2)
+    ];
+    const boltGuideLine2P1 = [
+        cuffModel.longRadius * Math.cos(cuffModel.alpha.radians - cuffModelInner.widthOffset.radians/2),
+        cuffModel.longRadius * Math.sin(cuffModel.alpha.radians - cuffModelInner.widthOffset.radians/2)
+    ];
+    const boltGuideLine2P2 = [
+        cuffModel.shortRadius * Math.cos(cuffModel.alpha.radians - cuffModelInner.widthOffset.radians/2),
+        cuffModel.shortRadius * Math.sin(cuffModel.alpha.radians - cuffModelInner.widthOffset.radians/2)
+    ];
+    completeCuffModel.models.leftBolts = makeTwoEvenlySpacedBolts(boltGuideLine1P1, boltGuideLine1P2);
+    completeCuffModel.models.rightBolts = makeTwoEvenlySpacedBolts(boltGuideLine2P1, boltGuideLine2P2);
+    /***** END RIVET HOLES *****/
 
-    // makerjs.model.rotate(completeCuffModel, 90);
-    // makerjs.model.zero(completeCuffModel);
+    /***** START RECENTER SHAPE *****/
+    // Model is way far out, move it close to origin
+    makerjs.model.rotate(completeCuffModel, 90);
+    makerjs.model.zero(completeCuffModel);
 
     this.models = {
         completeCuffModel: completeCuffModel
     }
+    /***** END RECENTER SHAPE *****/
 
+    /***** START DESIGN *****/
+    // Now make the design and clamp it to the inner/safe arc we built
     var bbox = makerjs.measure.modelExtents(completeCuffModel);
     var totalWidth = bbox.high[0] - bbox.low[0];
     var totalHeight = bbox.high[1] - bbox.low[1];
-    // console.log(totalWidth);
-    // console.log(totalHeight);
 
+    // this is some insane terrible logic to preserve the recentering we did above
+    // and just use the inner shape as the intersection clamp for our design
     var cuffClone = makerjs.model.clone(completeCuffModel);
-    cuffClone.models['cuffModel'] = null;
+    var cuffModelInnerClone = cuffClone.models.cuffModelInner;
+    cuffClone.models = { c: cuffModelInnerClone }
+
     this.models.design =
        makerjs.model.combineIntersection(
            new InnerDesign(totalHeight, totalWidth, seed, buffer_width, hash_width),
            cuffClone
        )
+    /***** END DESIGN *****/
 
+    /***** START CLEANUP *****/
+    // now take out the original inner cuff model, not actually used as a cut
     this.models.completeCuffModel.models.cuffModelInner = null;
 
-    // console.log(this.models.design)
-    // console.log(this)
-
     this.units = makerjs.unitType.Inch;
+    /***** END CLEANUP *****/
 }
 
 function Cuff(
@@ -168,8 +203,6 @@ function Cuff(
     var YPadding = 0.3;
 
     var DesignBuffer = sideBufferBottom - sideBufferTop;
-
-    var MillimeterToInches = 0.0393701;
 
     var points = [
         [0, 0], 
@@ -216,8 +249,8 @@ function InnerDesign(
     var lastNoise2 = simplex.noise2D(100.5, 10.5) * 10;
 
     var models = {};
-    var i = -5 ;
-    while (i <= width) {
+    var i = -width ;
+    while (i <= width*2) {
       var newNoise1 = (simplex.noise2D(500, (i-100)*1.0/200)+1)*0.1;
       var newNoise2 = (simplex.noise2D(500, (i-100)*1.0/200)+1)*0.25;
       
