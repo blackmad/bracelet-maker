9912e369fc644e3dde0bf5140c625ff8e201d08e
David Blackman <david@blackmad.com>
2019-05-27 20:42:38 -0400

    start with design

--- frontend/new-playground/frontend.js
+++ frontend/new-playground/frontend.js
@@ -9,24 +9,35 @@ import {InnerDesignCircles} from '../inner-design-circles.js';
 let playground = null;
 
 function attachHandlers() {
+    const possibleDesigns = [InnerDesignVoronoi, InnerDesignHashmarks, InnerDesignCircles]
+    const possibleDesignNameMap = {}
+    possibleDesigns.forEach(d => { possibleDesignNameMap[d.name] = d })
+
+    function setDesign(designClass) {
+        const modelMaker = function(options) {
+            return ConicCuffOuter(designClass)(options);
+        }
+
+        playground = new DavidsPlayground({modelMaker: modelMaker, subModels: [ConicCuffOuter, designClass]}).rerender();
+    }
+
+    possibleDesigns.forEach(d => {
+        console.log(d.name);
+        console.log(window.location.hash);
+        if (window.location.hash.indexOf(d.name) > 0) {
+            console.log('setting to ' + d)
+            setDesign(d);
+        }
+    });
+
     $('.designButton').click(function(button) {
         const design = button.target.value;
         var innerDesignClass = null;
-        if (design == 'InnerDesignVoronoi') {
-            innerDesignClass = InnerDesignVoronoi;
-        } else if (design == 'InnerDesignHashmarks') {
-            innerDesignClass = InnerDesignHashmarks;
-        } else if (design == 'InnerDesignCircles') {
-            innerDesignClass = InnerDesignCircles;
+        if (possibleDesignNameMap[design]) {
+            setDesign(possibleDesignNameMap[design]);
         } else {
             throw "can't interpret design: " + design;
         }
-
-        const modelMaker = function(options) {
-            return ConicCuffOuter(innerDesignClass)(options);
-        }
-
-        playground = new DavidsPlayground({modelMaker: modelMaker, subModels: [ConicCuffOuter, innerDesignClass]}).rerender();
     })
 }
 
